c80a4739e40880813dd9129dca28673b8754a3d9
Gus Cost <guscost@gmail.com>
2019-08-03 17:22:21 -0400

    Update models.js

--- models.js
+++ models.js
@@ -1,90 +1,105 @@
 module.exports = () => {
-     const Sequelize = require("sequelize");
-     const db = new Sequelize ("auth", "annaaombe","",{
-         host: "127.0.0.1",
-         dialect: "postgres",
-         logging: false
-     });
-     return{
-         db,
-         User: db.define("user", {
-             id:{
-                type: Sequelize.INTEGER.UNSIGNED,
-                autoIncrement: true,
-                primaryKey: true
-             },
-             username: Sequelize.STRING,
-             password: Sequelize.STRING,
-             salt: Sequelize.STRING(3)
-         }),
-         Donor: db.define("donors",{
-            id:{
-               type: Sequelize.INTEGER.UNSIGNED,
-               autoIncrement: true,
-               primaryKey: true
-            },
-            name: Sequelize.STRING,
-            contact: Sequelize.STRING,
-            city: Sequelize.STRING,
-            state: Sequelize.STRING,
-            zipcode: Sequelize.STRING
-        }),
-        Recipient: db.define("recipients",{
-            id:{
-               type: Sequelize.INTEGER.UNSIGNED,
-               autoIncrement: true,
-               primaryKey: true
-            },
-            name: Sequelize.STRING,
-            contact: Sequelize.STRING,
-            city: Sequelize.STRING,
-            state:Sequelize.STRING,
-            zipcode: Sequelize.STRING
-        }),
-        Category: db.define("category",{
-            id:{
-                type: Sequelize.INTEGER.UNSIGNED,
-                autoIncrement: true,
-                primaryKey: true
-            },
-            name: Sequelize.STRING,
+    const Sequelize = require("sequelize");
+    const db = new Sequelize ("auth", "annaaombe","",{
+        host: "127.0.0.1",
+        dialect: "postgres",
+        logging: false
+    });
+     
+    const User = db.define("user", {
+      id:{
+        type: Sequelize.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      username: Sequelize.STRING,
+      password: Sequelize.STRING,
+      salt: Sequelize.STRING(3)
+    });
 
-        }),
-        Product: db.define("product",{
-            id:{
-                type: Sequelize.INTEGER.UNSIGNED,
-                autoIncrement: true,
-                primaryKey: true
-            },
-            category_id: {
-                type: Sequelize.INTEGER,
-                allowNull: false,
-                references: {
-                    model: 'Category',
-                    key: 'id'
-                }
-            },
-            productname: Sequelize.STRING,
-            productcondition: Sequelize.STRING,
-            productcondition: Sequelize.STRING,
-            productimage: Sequelize.STRING
-        }),
-        Quantity: db.define("quantity",{
-            id:{
-               type: Sequelize.INTEGER.UNSIGNED,
-               autoIncrement: true,
-               primaryKey: true
-            }
-        }),
-        Order: db.define("order",{
-            id:{
-               type: Sequelize.INTEGER.UNSIGNED,
-               autoIncrement: true,
-               primaryKey: true
-            }
-        }),
+    const Donor = db.define("donors",{
+      id:{
+         type: Sequelize.INTEGER.UNSIGNED,
+         autoIncrement: true,
+         primaryKey: true
+      },
+      name: Sequelize.STRING,
+      contact: Sequelize.STRING,
+      city: Sequelize.STRING,
+      state: Sequelize.STRING,
+      zipcode: Sequelize.STRING
+    });
+
+    const Recipient = db.define("recipients",{
+      id:{
+         type: Sequelize.INTEGER.UNSIGNED,
+         autoIncrement: true,
+         primaryKey: true
+      },
+      name: Sequelize.STRING,
+      contact: Sequelize.STRING,
+      city: Sequelize.STRING,
+      state:Sequelize.STRING,
+      zipcode: Sequelize.STRING
+    });
+
+    const Category = db.define("category",{
+      id:{
+          type: Sequelize.INTEGER.UNSIGNED,
+          autoIncrement: true,
+          primaryKey: true
+      },
+      name: Sequelize.STRING,
+
+    });
+
+    const Product = db.define("product",{
+      id:{
+          type: Sequelize.INTEGER.UNSIGNED,
+          autoIncrement: true,
+          primaryKey: true
+      },
+      category_id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+              model: 'Category',
+              key: 'id'
+          }
+      },
+      productname: Sequelize.STRING,
+      productcondition: Sequelize.STRING,
+      productcondition: Sequelize.STRING,
+      productimage: Sequelize.STRING
+    });
+
+    const Quantity = db.define("quantity",{
+      id:{
+          type: Sequelize.INTEGER.UNSIGNED,
+          autoIncrement: true,
+          primaryKey: true
+      }
+    });
+
+    const Order = db.define("order",{
+      id:{
+         type: Sequelize.INTEGER.UNSIGNED,
+         autoIncrement: true,
+         primaryKey: true
+      }
+    });
+     
+    return {
+        db,
+        User,
+        Donor,
+        Recipient,
+        Category,
+        Product,
+        Quantity,
+        Order,
         init: function(){
             db.sync({force: true});
         }
-     }
- }
+    }
+}
