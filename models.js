21fd5b64bd6de72f780d69409fc47105407de18d
Anna <48394404+World-Cup@users.noreply.github.com>
2019-08-03 16:38:12 -0400

    foreign key setup

---
+++ models.js
@@ -0,0 +1,88 @@
+ module.exports=()=>{
+     const Sequelize = require("sequelize");
+     const db = new Sequelize ("auth", "annaaombe","",{
+         host:"127.0.0.1",
+         dialect: "postgres",
+         logging: false
+     });
+     return{
+         db,
+         User: db.define("user", {
+             id:{
+                type: Sequelize.INTEGER.UNSIGNED,
+                autoIncrement: true,
+                primaryKey: true
+             },
+             username: Sequelize.STRING,
+             password: Sequelize.STRING,
+             salt: Sequelize.STRING(3)
+         }),
+         Donor: db.define("donors",{
+            id:{
+               type: Sequelize.INTEGER.UNSIGNED,
+               autoIncrement: true,
+               primaryKey: true
+            },
+            name: Sequelize.STRING,
+            contact: Sequelize.STRING,
+            city: Sequelize.STRING,
+            state: Sequelize.STRING,
+            zipcode: Sequelize.STRING
+        }),
+        Recipient: db.define("recipients",{
+            id:{
+               type: Sequelize.INTEGER.UNSIGNED,
+               autoIncrement: true,
+               primaryKey: true
+            },
+            name: Sequelize.STRING,
+            contact: Sequelize.STRING,
+            city: Sequelize.STRING,
+            state:Sequelize.STRING,
+            zipcode: Sequelize.STRING
+        }),
+        Product: db.define("product",{
+            id:{
+                type: Sequelize.INTEGER.UNSIGNED,
+                autoIncrement: true,
+                primaryKey: true
+            },
+            category_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'Category',
+                    key: 'id'
+                }
+            },
+            productname: Sequelize.STRING,
+            productcondition: Sequelize.STRING,
+            productcondition: Sequelize.STRING,
+            productimage: sequelize.STRING
+        }),
+        Category: db.define("category",{
+            id:{
+                type: Sequelize.INTEGER.UNSIGNED,
+                autoIncrement: true,
+                primaryKey: true
+            },
+            name: Sequelize.STRING,
+
+        }),
+        Quantity: db.define("quantity",{
+            id:{
+               type: Sequelize.INTEGER.UNSIGNED,
+               autoIncrement: true,
+               primaryKey: true
+            }
+        }),
+        Order: db.define("order",{
+            id:{
+               type: Sequelize.INTEGER.UNSIGNED
+            }
+        }),
+        init: function(){
+            db.sync();
+        }
+     }
+ }
